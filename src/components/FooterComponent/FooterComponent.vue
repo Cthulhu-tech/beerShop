f9df3e0e85a149ce972b991bd80893d2066127b6
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-01-07 03:27:50 +0400

    create footer and nav component

---
+++ src/components/FooterComponent/FooterComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    
+  </footer>
+</template>
+
+<script lang="ts">
+  export default {
+    setup() {
+      
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @import './FooterComponent.scss';
+</style>
+  
--- src/components/NavComponent/NavComponent.vue
+++ src/components/NavComponent/NavComponent.vue
@@ -1,13 +1,26 @@
 <template>
-  <nav></nav>
+  <nav>
+    <router-link :to="{name: 'home'}">Главная</router-link>
+    <router-link :to="{name: 'beer'}">Пиво</router-link>
+    <router-link :to="{name: 'cocktail'}">Коктели</router-link>
+    <router-link :to="{name: 'cognac'}">Коньяк</router-link>
+    <router-link :to="{name: 'whiskey'}">Виски</router-link>
+    <router-link :to="{name: 'wine'}">Вино</router-link>
+    <router-link :to="{name: 'snack'}">Закуски</router-link>
+  </nav>
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-class-component';
-
-export default class NavComponent extends Vue {}
+  export default {
+    setup() {
+      
+      return {
+        
+      }
+    }
+  }
 </script>
 
 <style scoped lang="scss">
-
+  @import './NavComponent.scss';
 </style>
