0db9852829f6b6a490402ef8a1fecb6ab897d73a
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-01-07 01:16:37 +0400

    init project

---
+++ src/components/NavComponent/NavComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav></nav>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class NavComponent extends Vue {}
+</script>
+
+<style scoped lang="scss">
+
+</style>
